45ce2e28421d1c3d604ad1a31b81d6c77f83fd09
Poojayy-7 <20poojachandnani@gmail.com>
2025-04-09 06:35:04 +0000

    Completed Spring LoanApplication 09/04 12:05

--- springapp/src/main/java/com/examly/springapp/model/LoanApplication.java
+++ springapp/src/main/java/com/examly/springapp/model/LoanApplication.java
@@ -1,8 +1,50 @@
 package com.examly.springapp.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class LoanApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanApplicationId;
+    private LocalDate submissionDate;
+    private int loanStatus;
+    private String farmLocation;
+    private String farmerAddress;
+    private double farmSizeInAcres;
+    private String farmPurpose;
+    private String file;
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="loanId")
+    private Loan loan;
+
+
+
+
+
+    
 
 }
--- springapp/src/main/java/com/examly/springapp/repository/LoanApplicationRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/LoanApplicationRepo.java
@@ -1,5 +1,11 @@
 package com.examly.springapp.repository;
 
-public interface LoanApplicationRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.examly.springapp.model.LoanApplication;
+
+@Repository
+public interface LoanApplicationRepo extends JpaRepository<LoanApplication,Long>{
 
 }
