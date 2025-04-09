ed6437c9830552e64bb9d65ef41f40965cf590fc
tarunsahu9399 <20it1062@mitsgwl.ac.in>
2025-04-09 13:13:32 +0000

    update exceptions

--- springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
@@ -1,7 +1,8 @@
 package com.examly.springapp.service;
 
-import com.examly.springapp.exception.UserAlreadyExistsException;
-import com.examly.springapp.exception.InvalidCredentialsException;
+
+import com.examly.springapp.exceptions.InvalidCredentialsException;
+import com.examly.springapp.exceptions.UserAlreadyExistsException;
 import com.examly.springapp.model.User;
 import com.examly.springapp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
