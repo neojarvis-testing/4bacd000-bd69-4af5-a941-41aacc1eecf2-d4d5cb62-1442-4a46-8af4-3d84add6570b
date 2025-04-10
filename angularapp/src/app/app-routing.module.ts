e047acc49d52f7ea28ab6a93bdde5db4b4d75fb1
PoojithaSankar <spoojitha54@gmail.com>
2025-04-10 07:28:28 +0000

    Feedback Frontend 10/04 01:00

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminviewfeedbackComponent } from './components/adminviewfeedback/adminviewfeedback.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'adminviewfeedback', component: AdminviewfeedbackComponent},
+  { path: 'useraddfeedback', component: UseraddfeedbackComponent},
+  { path: 'userviewfeedback', component: UserviewfeedbackComponent},
+  { path: '', redirectTo: '/useraddfeedback', pathMatch: 'full'}, // Default route
+  { path: '**', redirectTo: '/useraddfeedback' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
