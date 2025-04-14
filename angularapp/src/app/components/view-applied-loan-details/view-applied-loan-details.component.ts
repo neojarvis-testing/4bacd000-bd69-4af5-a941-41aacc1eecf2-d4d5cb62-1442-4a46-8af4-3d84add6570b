99111efb568e70e83cabdb8bfa4eb05805910f60
tarunsahu9399 <20it1062@mitsgwl.ac.in>
2025-04-14 04:32:14 +0000

    14/04: View Applied Loan Added For Both Admin and User

--- angularapp/src/app/components/view-applied-loan-details/view-applied-loan-details.component.ts
+++ angularapp/src/app/components/view-applied-loan-details/view-applied-loan-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoanApplication } from 'src/app/models/loanapplication.model';
+import { LoanService } from 'src/app/services/loan.service';
 
 @Component({
   selector: 'app-view-applied-loan-details',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAppliedLoanDetailsComponent implements OnInit {
 
-  constructor() { }
+
+  loanApplication:LoanApplication ={
+   loan:{},
+    submissionDate: "",
+    loanStatus: 0,
+    farmLocation: "",
+    farmerAddress: "",
+    farmSizeInAcres: 0,
+    farmPurpose: "",
+    file: ""
+  
+  } ;
+
+  loanAppId:number;
+  role:string ="";
+
+
+  constructor(private loanService :LoanService, private router:Router, private activateRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loanAppId = parseInt(this.activateRoute.snapshot.paramMap.get("id"));
+    this.getLoanApp();
+    this.role = sessionStorage.getItem("userRole");
+    console.log(this.role)
+    
+  }
+
+  getLoanApp(){
+    this.loanService.getLoanApplicationById(this.loanAppId).subscribe(data=>{
+      this.loanApplication = data;
+      console.log(this.loanApplication)
+    })
+  }
+
+  backButtonForUser(){
+    this.router.navigate(['/userappliedloan'])
+  }
+
+  backButtonForAdmin(){
+    this.router.navigate(['/requestedloan'])
   }
 
 }
